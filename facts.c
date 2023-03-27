2c74adc3c0aaa49caa7719dbaab2972a1c1a69bc
annaannmathew <85623708+annaannmathew@users.noreply.github.com>
2023-03-27 14:07:53 +0530

    Add files via upload

---
+++ facts.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+    char buf[100];
+    int k;
+    socklen_t len;
+    int sock_desc,temp_sock_desc;
+    struct sockaddr_in server,client;
+    sock_desc = socket(AF_INET,SOCK_STREAM,0);
+    if(sock_desc==-1)
+    {
+	printf("Error in socket creation");
+    }
+    server.sin_family=AF_INET;
+    server.sin_addr.s_addr=INADDR_ANY;
+    server.sin_port=3003;
+    client.sin_family=AF_INET;
+    client.sin_addr.s_addr=INADDR_ANY;
+    client.sin_port=3003;
+    k=bind(sock_desc,(struct sockaddr*)&server,sizeof(server));
+    if(k==-1)
+    {
+	printf("Error in binding...");
+    }
+    k=listen(sock_desc,5);
+    if(k==-1)
+    {
+	printf("Error in listening");
+    }
+    len=sizeof(client);
+    temp_sock_desc=accept(sock_desc,(struct sockaddr*)&client,&len);
+    if(temp_sock_desc==-1)
+    {
+	printf("Error in Temporary socket creation");
+    }
+    k=recv(temp_sock_desc,buf,100,0);
+    if(k==-1)
+    {
+	printf("Error in receiving");
+    }
+    int n=atoi(buf);
+    int i,fact=1;
+    for(i=1;i<=n;i++)
+    {
+	fact=fact*i;
+    }
+    printf("Factorial of the number sent from client is:%d\n",fact); 
+    close(temp_sock_desc);
+    return 0;
+}
