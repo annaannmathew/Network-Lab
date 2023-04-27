523a0b495732f5b8b22f010db1fad6a3ae213f1f
annaannmathew <85623708+annaannmathew@users.noreply.github.com>
2023-04-27 11:33:07 +0530

    Add files via upload

---
+++ distance.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+struct Link
+{
+    int hop,dest,wt;
+};
+
+struct Network
+{
+    int H,L;
+    struct Link*link;
+};
+
+void main()
+{
+    int H,L,S,i,j,dist[H];
+    printf("Distance Vector Routing using Bellmen Ford Algorithm:\nEnter number of hops\n");
+    scanf("%d",&H);
+    printf("Enter number of links\n");
+    scanf("%d",&L);
+    printf("Enter source\n");
+    scanf("%d",&S);
+    struct Network*n=(struct Network*)malloc(sizeof(struct Network));
+    n->H=H;
+    n->link=(struct Link*)malloc(n->L *sizeof(struct Link));
+    for(i=0;i<L;i++)
+    {
+	printf("\nLink%d:Enter Source,Destination and Weight \n",i+1);
+	scanf("%d",&n->link[i].hop);
+	scanf("%d",&n->link[i].dest);
+	scanf("%d",&n->link[i].wt);
+    }
+    for(i=0;i<H;i++)
+	dist[i]=INT_MAX;
+    dist[S]=0;
+    for(i=1;i<H;i++)
+    {
+	for(j=0;j<L;j++)
+	{
+	    int u=n->link[j].hop;
+	    int v=n->link[j].dest;
+	    int wt=n->link[j].wt;
+	    if(dist[u]+wt<dist[v])
+		dist[v]=dist[u]+wt;
+	}
+    }
+    for(i=0;i<L;i++)
+    {
+	int u=n->link[i].hop;
+	int v=n->link[i].dest;
+	int wt=n->link[i].wt;
+	if(dist[u]+wt<dist[v])
+	    printf("Network contains negative weight cycle\n");
+    }
+    printf("\nHop\tDistance from source\n");
+    for(i=0;i<H;i++)
+    {
+	printf("%d\t%d\n",i,dist[i]);
+    }
+}
